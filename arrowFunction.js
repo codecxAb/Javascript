d3b1db139b2099506963de08f67db0f18224f5cf
Codecx <97624935+codecxAb@users.noreply.github.com>
2024-02-12 02:40:12 +0000

    lvl 3

---
+++ arrowFunction.js
@@ -0,0 +1,19 @@
+ 
+
+ let sum = (a,b) => a+b;
+   
+
+
+ console.log(sum(2,3));
+ console.log(sum(5,6));
+
+ 
+
+ let splitIntoArray = (str) => {
+    
+    const wordarr = str.split(' ');
+    return wordarr;
+}
+let str = "this string should be splitted into a array of words";
+console.log(splitIntoArray(str));
+
