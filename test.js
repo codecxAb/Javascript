3070f68c557b4df249b41a1c56e68a12e19a66a4
CodecxAb <97624935+codecxAb@users.noreply.github.com>
2024-11-30 13:43:30 +0000

    updated code

---
+++ test.js
@@ -0,0 +1,11 @@
+
+function orderTea(chaiType){
+    function sucessOrder(){
+        return `the order of ${chaiType} is successful`
+    }
+    return sucessOrder()
+}
+
+ let res = orderTea("greenTea");
+ console.log(res);
+
